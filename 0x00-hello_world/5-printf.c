de6f44ff2fe49957437a039a423c00a251ffce29
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 02:20:57 -0700

    m

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * Main - printing 
+ *
+ * Description: printing a string using the printf function
+ *
+ * Return: Always 0 (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return 0;
+}
