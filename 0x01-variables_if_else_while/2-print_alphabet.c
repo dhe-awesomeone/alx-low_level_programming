f27d9ad2e70810be435c25274a1751fb28a03667
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 15:22:56 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program to print alphabets in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	ch = "abcdefghijklmnopqrstuvwxyz";
+	
+	putchar(ch);
+	return 0;
+}
