32540434b59ffda6060e2aa1f892dc8319f47ebc
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 02:14:00 -0700

    k

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Printing
+ * 
+ * Description: c program to print string using puts function
  *
- * DEscription: c program to print string using puts function 
- *
- * Return: Always 0 (success)
+ * Return: Always 0 (successful)
  */
-int main(void) 
+int main(void)
 {	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
