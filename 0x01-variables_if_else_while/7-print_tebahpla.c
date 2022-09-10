2499ac193f7f6cc1561806c391074991272eadab
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 21:36:48 -0700

    l

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - code to peint in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
