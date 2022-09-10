7368f820e6b85c189866ce38a39367dc494fab58
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 20:57:37 -0700

    d

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prin numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	x = 0;
+	while (x <= 9)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+	return (0);
+}
