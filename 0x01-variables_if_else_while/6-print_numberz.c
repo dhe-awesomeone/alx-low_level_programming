14c2119cbd9b499abf51b02382e5703f354a98f4
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 21:06:15 -0700

    k

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print single digit of base 10
+ *
+ * Description: Use only putchar statement and avoid any char vairable
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+	
+	x = 0;
+	while (x <= 9)
+	{
+		putchar(x);
+		x++;
+	}
+	return (0);
+}
