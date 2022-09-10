250d85622be43636be6254392fc76e69d5f55c9f
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 21:45:46 -0700

    k

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal 
+ *
+ * Return: Always 0 (success)
+ */
+int main(main)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+	for (x = 'a'; x <= 'f'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n')
+	return (0);
+}
