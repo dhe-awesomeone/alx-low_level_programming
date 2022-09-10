bfcf1fc395997be2ab83f6ad6f23987da0eab4f9
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-09 22:04:59 -0700

    l

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print numbeers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 58; x++)
+	{
+		putchar(x);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');								}									}
+	putchar('\n');
+	return (0);
+}
