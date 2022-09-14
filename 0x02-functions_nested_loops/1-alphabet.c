d61df98da799ddefbc6623c34471fbf58e51b478
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-13 18:11:54 -0700

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - program to print letters using putchar and funciotns
+ *
+ * Return: Always 0 (success)
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
