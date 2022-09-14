3641e677d08ec3117308eb3bdaf3c091a9cf4161
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-13 17:41:26 -0700

    k

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - c program to print _putchar
  *
@@ -6,7 +6,14 @@
  */
 int main(void)
 {
-	putchar(_putchar);
+	char s[] = "_putchar";
+	
+	int x;
+
+	for (x = 0; x < 8; x++)
+	{
+		_putchar(s[x]);
+	}
 	putchar('\n');
 	return (0);
 }
