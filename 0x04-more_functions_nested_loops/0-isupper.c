47157bec2f07a656aed9fe9eb09414222581ccf9
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-15 20:42:05 -0700

    l

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - code for upper case
+ *
+ * @c: variable
+ *
+ * Return: 1 upper case 0 lowercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' || c <= 'Z')
+	{
+		return(1);
+	}
+	else
+	{
+		return(0);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,8 @@
+0x04 C - more_functions_nested_loops
+
+By Agamah Alvin
+
+Ongoing project - started 09/15/2022, must end by 09/16/2022 (in 1 day) - you're done with 0% of task
+
+Checker was realease
+
