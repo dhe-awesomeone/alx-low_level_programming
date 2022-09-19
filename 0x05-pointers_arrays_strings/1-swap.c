7c388c3b061e7cfc532eb2d4438a1e0e3ef3e83f
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-19 16:13:47 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - function to swap value of variables
+ *
+ * @a: pinter variable
+ * @b: pionter variable
+ */
+void swap_int(int *a, int *b)
+{
+	int c = 0;
+	c = *a;
+	*a = *b;
+	*b = c;
+}
