b47c520fac2fa154bda0f8fc193360607b6ae2f2
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-19 16:19:56 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+#include <stdio.h>
+
+/**
+ *
+ *  * _strlen - print size of string parameter
+ *
+ *   * Return: length of string
+ *
+ *    * @s: pointer to array of characters
+ *
+ *     */
+
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (*(s + a) != '\0')
+	{
+		a++;									}
+	return (a);
+}
