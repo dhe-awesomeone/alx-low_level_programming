2d8b7536ac35608a1c742276706c7151fc80ce94
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-20 14:54:19 -0700

    putchar with pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _puts - function to print out a string using pointers
+ *
+ * Return: always 0
+ */
+void _puts(char *str)
+{
+	while (*(str + a) != '\0')
+	{
+		_putchar(*(str + a));
+		a++;
+	}
+	return (0);
+}
