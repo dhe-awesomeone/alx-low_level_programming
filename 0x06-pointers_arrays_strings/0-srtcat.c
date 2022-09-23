ee926a912b71be366ef18541d09bfddb5919b386
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-22 19:29:14 -0700

    srt

--- 0x06-pointers_arrays_strings/0-srtcat.c
+++ 0x06-pointers_arrays_strings/0-srtcat.c
@@ -1,24 +1,20 @@
 #include "main.h"
 
 /**
- * _strcat -> Function that concatenate steings
- * @dest: first string
- * @src: second string
- * Return: a concatenated string.
+ * *_strcat - concatenates @src to @dest
+ * @src: the source string to append to @dest
+ * @dest: the destiation string to be concatenated upon
+ * Return:pointer to the resulting string
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, i;
+	int index = 0;
+	int dest_len = 0;
 
-	while (dest[len])
-		len++;
-
-	for (i = 0; src[i] != '\0'; i++)
-	{
-		dest[len] = src[i];
-		len += 1;
-	}
-	dest[len] = '\0';
+	while (dest[index++])
+		dest_len++;
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 	return (dest);
-
 }
