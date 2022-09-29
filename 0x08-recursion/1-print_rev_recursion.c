2e78ee6e1b84af85c016174787a5c8040a05b420
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-29 02:14:35 -0700

    print_rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,14 +6,6 @@ void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		char strrev();
-
-		char rev = strrev(*s);
-		_putchar(rev);
 		_print_rev_recursion(s + 1);
-	}
-	else
-	{
-		_putchar('\n');
-	}
+		_putchar(*s);
 }
