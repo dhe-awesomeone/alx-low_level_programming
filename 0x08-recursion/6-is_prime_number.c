414752de3afe857b96e234d3072bae05d966ca63
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-29 20:56:46 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,25 @@
 #include "main.h"
-
+/**
+ * is_prime_number - function
+ * @n: variale
+ * Return: (1) 
+ */
 int is_prime_number(int n)
 {
-	int prim;
-	if 
+	int p;
+	
+	if ((n / p) == 1)
+	{
+		return (1);
+	}
+	if (n == 1)
+	{
+		return (1);
+	}
+	else 
+		return (0);
+
+	return (is_prime_number(n, p + 1);
+}
+
+
