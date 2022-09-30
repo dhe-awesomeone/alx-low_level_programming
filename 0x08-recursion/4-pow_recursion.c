8cfe580fc76dad970ee0d83288d1c45a5df9d3ff
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-29 19:16:56 -0700

    power

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _pow_recursion - function to print power
+ * _pow_recursion - function to print power
  * @x: variable
  * @y: variable
  * Return: -1 (error), 1 (y is 0)
@@ -11,6 +11,6 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	if (y == 0)
 		return (1);
-	else 
+	else
 		return (x * _pow_recursion(x, y - 1));
 }
