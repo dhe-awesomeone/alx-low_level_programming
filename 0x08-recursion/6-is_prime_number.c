8a4042a3627dca59832a594c8102f103f46df790
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-29 21:09:43 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,26 +1,37 @@
 #include "main.h"
-/**
- * is_prime_number - function
- * @n: variale
- * @p: variable
- * Return: (1) 
- */
-int is_prime_number(int n, int p)
-{
-	int p;
-	
-	if ((n / p) == 1)
-	{
-		return (1);
-	}
-	if (n == 1)
-	{
-		return (1);
-	}
-	else 
-		return (0);
 
-	return (is_prime_number(n, p + 1));
+/**
+ * can_divide - checks if a number and divide another
+ * @n: the number to be divided
+ * @d: the divisor
+ *
+ * Return: 1 or 0
+ */
+int can_divide(int n, int d)
+{
+	if (n % d == 0)
+		return (0);
+	if (d == n / 2)
+		return (1);
+
+	return (can_divide(n, d + 1));
 }
 
+/**
+ * is_prime_number - checks if number is prime
+ * @n: the number
+ *
+ * Return: 1 if the input integer is a prime number, otherwise return 0
+ */
+int is_prime_number(int n)
+{
+	int d = 2;
 
+	if (n <= 1)
+		return (0);
+
+	if (n == 2 || n == 3)
+		return (1);
+
+	return (can_divide(n, d));
+}
