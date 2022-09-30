05290f021d38cc4b2d01accde23028d2fe824ce8
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-29 17:10:54 -0700

    strlen

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -8,7 +8,7 @@ int _strlen_recursion(char *s)
 	if (*s)
 	{
 		_putchar(*s);
-		//_print_rev_recursion(s + 1);
+		/*_print_rev_recursion(s + 1);*/
 		return (1 + _strlen_recursion(s + 1));
 	}
 	else
