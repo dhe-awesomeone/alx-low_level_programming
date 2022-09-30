440bf682c6573c69631c5d64f2d6ae25bf80d8d9
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-09-29 20:37:38 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _sqrt_recursion - function to get square root
+ * _get_sqrt - function to get square root
  * @r: variable for square root
  * @n: variable
  * Return: r
@@ -11,14 +11,13 @@ int _get_sqrt(int n, int r)
 		return (r);
 	if (r == n / 2)
 		return (-1);
-	else 
+	else
 		return (_get_sqrt(n, r + 1));
 }
 
 /**
  * _sqrt_recursion - function to print square root of a number
  * @n: variable
- * @r: variable
  * Return: r
  */
 int _sqrt_recursion(int n)
