af7fb596b8310926cba930237d1fb0ac63654663
dhe-awesomeone <alvin.agamah@stu.cu.edu.nd>
2022-10-05 21:57:55 -0700

    push

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of arguments passed to this program
+ * @argv: an array of pointers to strings
+ *
+ * Return: Succesful execution 0, failed execution 1.
+ */
+int main(int argc, char *argv[])
+{
+	int i, sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", argc - 1);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if ((*argv[i]) >= '0' && (*argv[i]) <= '9')
+		{
+			sum += atoi(argv[i]);
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
